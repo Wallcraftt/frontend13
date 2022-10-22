4d0f7a2b131f58cdcde5fd6be97d6c4ad6c8afaf
Wallcraft <102182020+Wallcraftt@users.noreply.github.com>
2022-10-22 12:41:39 +0400

    end

---
+++ js1/main.js
@@ -0,0 +1,13 @@
+let myName = 'goga'
+let text = 'my name is'
+let  myHobby = 'programmer'
+let yearOfBirth = 2005
+let currentYear = 2022
+let oldHobby = 'old and my hobby is'
+
+const myNameIs = text + myName 
+const myAge = yearOfBirth - currentYear 
+const hobby = oldHobby + myHobby
+
+console.log(myNameIs, currentYear - yearOfBirth, hobby)
+
