b9f77ddef51c4b034e88b09b182d778a4fdc1aba
Wallcraft <102182020+Wallcraftt@users.noreply.github.com>
2022-10-23 19:12:34 +0400

    Update main.js

--- js1/main.js
+++ js1/main.js
@@ -1,13 +1,14 @@
-let myName = 'goga'
-let text = 'my name is'
-let  myHobby = 'programmer'
-let yearOfBirth = 2005
-let currentYear = 2022
-let oldHobby = 'old and my hobby is'
+let myName = 'goga';
+let text = 'my name is';
+let  myHobby = 'programmer';
+let yearOfBirth = 2005;
+let currentYear = 2022;
+let oldHobby = 'old and my hobby is';
 
-const myNameIs = text + myName 
-const myAge = yearOfBirth - currentYear 
-const hobby = oldHobby + myHobby
+const myNameIs = text + myName ;
+const myAge = `my age ${currentYear - yearOfBirth} `;
+const hobby = oldHobby + myHobby;;
 
-console.log(myNameIs, currentYear - yearOfBirth, hobby)
+
+console.log(myNameIs, myAge, hobby)
 
